a4793eda238d33024ca6313f9284e8adf4c0549c
cellle <91531988+cellle@users.noreply.github.com>
2022-09-12 15:13:23 +0200

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { BsChevronLeft } from "react-icons/bs";
+import { VscColorMode } from "react-icons/vsc";
+import { CgLogOut } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
+
+function Header({ title }: { title: string }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="grid items-center grid-cols-3 p-4 text-xl bg-red-300 text-zinc-700 ">
+      {title !== "home" && <BsChevronLeft className="mr-auto" />}
+      {title === "home" && (
+        <CgLogOut
+          className="mr-auto"
+          onClick={() => {
+            navigate("/login");
+          }}
+        />
+      )}
+      <h1 className="col-start-2 font-light tracking-widest uppercase place-self-center">
+        {title}
+      </h1>
+      {title === "home" && <VscColorMode className="ml-auto" />}
+    </div>
+  );
+}
+
+export default Header;
