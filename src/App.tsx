9a91bc4ec801e055123366213b5da52eb50d239a
cellle <91531988+cellle@users.noreply.github.com>
2022-09-13 08:59:51 +0200

    fix layout

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import useCurrentRoute from "./hooks/useCurrentPath";
+
+function App() {
+  const { currentPath } = useCurrentRoute();
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header title={currentPath} />
+      <div className="h-full">
+        <Outlet />
+      </div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
