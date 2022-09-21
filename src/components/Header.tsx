8770b5b9804e89d8e995b6843aa0af50dc0af26b
cellle <91531988+celinerh@users.noreply.github.com>
2022-09-21 22:31:09 +0200

    add dark mode styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ function Header({ title }: { title: string }) {
   const navigate = useNavigate();
 
   return (
-    <div className="grid items-center grid-cols-3 p-4 text-xl bg-red-300 text-zinc-700 ">
+    <div className="grid items-center grid-cols-3 p-4 text-xl bg-red-300 text-zinc-700 dark:bg-secondary dark:text-white">
       {title !== "home" && <BsChevronLeft className="mr-auto" />}
       {title === "home" && (
         <CgLogOut
@@ -17,7 +17,7 @@ function Header({ title }: { title: string }) {
           }}
         />
       )}
-      <h1 className="col-start-2 font-light tracking-widest uppercase place-self-center">
+      <h1 className="col-start-2 tracking-widest uppercase font-extralight place-self-center">
         {title}
       </h1>
       {title === "home" && <VscColorMode className="ml-auto" />}
