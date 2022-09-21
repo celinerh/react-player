7c41cee72dd99d0bda460f31c4cd11f12eb2b060
cellle <91531988+celinerh@users.noreply.github.com>
2022-09-21 22:31:27 +0200

    use components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,34 @@
+import AlbumCard from "../../components/AlbumCard";
+import Heading from "../../components/Heading";
+import Title from "../../components/Title";
 import useNewReleases from "../../hooks/useNewReleases";
 
 function Home() {
   const { newReleases } = useNewReleases();
+  console.log(newReleases);
 
   return (
     <div>
-      {newReleases &&
-        newReleases.albums.items.map((item: any) => (
-          <div key={item.id}>
-            <h1>{item.name}</h1>
-            <div>
-              {item.artists.map((artist: any) => (
-                <p key={artist.id}>{artist.name}</p>
-              ))}
-            </div>
-          </div>
-        ))}
+      <Heading title="All Albums" />
+      <Title title="New Releases" />
+      <div className="grid grid-cols-2 gap-6">
+        {newReleases &&
+          newReleases.albums.items.map((album: any) => (
+            // <div key={item.id}>
+            //   <h1>{item.name}</h1>
+            //   <div>
+            //     {item.artists.map((artist: any) => (
+            //       <p key={artist.id}>{artist.name}</p>
+            //     ))}
+            //   </div>
+            // </div>
+            <AlbumCard
+              key={album.id}
+              title={album.name}
+              imageSource={album.images[0].url}
+            />
+          ))}
+      </div>
     </div>
   );
 }
