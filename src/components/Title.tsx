9fcccfa6ba73821c9a271e8185fd8508aa9703e8
cellle <91531988+celinerh@users.noreply.github.com>
2022-09-21 22:30:35 +0200

    add title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Title({ title }: { title: string }) {
-  return <h2 className="font-bold text-secondary">{title}</h2>;
+  return <h2 className="font-bold text-secondary dark:text-white">{title}</h2>;
 }
 
 export default Title;
