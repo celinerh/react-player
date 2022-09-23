036bcaba4b606ec8d17495ebb4fb88f0c2933ca8
cellle <91531988+cellle@users.noreply.github.com>
2022-09-23 12:50:15 +0200

    change icons and  title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,15 @@
-import { BsChevronLeft } from "react-icons/bs";
-import { VscColorMode } from "react-icons/vsc";
-import { CgLogOut } from "react-icons/cg";
+import { IoIosArrowBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
+import { IoSearchOutline, IoLogOutOutline } from "react-icons/io5";
 
 function Header({ title }: { title: string }) {
   const navigate = useNavigate();
 
   return (
-    <div className="grid items-center grid-cols-3 p-4 text-xl bg-red-300 text-zinc-700 dark:bg-secondary dark:text-white">
-      {title !== "home" && <BsChevronLeft className="mr-auto" />}
-      {title === "home" && (
-        <CgLogOut
+    <div className="grid items-center grid-cols-3 p-4 text-xl text-zinc-700 dark:bg-secondary dark:text-white">
+      {title !== "featured" && <IoIosArrowBack className="mr-auto" />}
+      {title === "featured" && (
+        <IoLogOutOutline
           className="mr-auto"
           onClick={() => {
             navigate("/login");
@@ -20,7 +19,7 @@ function Header({ title }: { title: string }) {
       <h1 className="col-start-2 tracking-widest uppercase font-extralight place-self-center">
         {title}
       </h1>
-      {title === "home" && <VscColorMode className="ml-auto" />}
+      <IoSearchOutline className="ml-auto" />
     </div>
   );
 }
