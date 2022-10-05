570fe725f9d49af26cdc64fc4da75dbc6b665a8a
cellle <91531988+cellle@users.noreply.github.com>
2022-10-05 13:48:47 +0200

    add player provider

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ import Categories from "./pages/categories";
 import Album from "./pages/album";
 import Playlist from "./pages/playlist";
 import Category from "./pages/category";
+import { PlayerProvider } from "./contexts/PlayerContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -21,20 +22,22 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <TokenProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<App />}>
-            <Route path="/albums" element={<Albums />} />
-            <Route path="/album/:id" element={<Album />} />
-            <Route path="/playlists" element={<Playlists />} />
-            <Route path="/playlist/:id" element={<Playlist />} />
-            <Route path="/featured" element={<Featured />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/category/:id" element={<Category />} />
-            <Route path="/playing" element={<Playing />} />
-          </Route>
-          <Route path="*" element={<Login />} />
-        </Routes>
+        <PlayerProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<App />}>
+              <Route path="/albums" element={<Albums />} />
+              <Route path="/albums/:id" element={<Album />} />
+              <Route path="/playlists" element={<Playlists />} />
+              <Route path="/playlists/:id" element={<Playlist />} />
+              <Route path="/featured" element={<Featured />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/categories/:id" element={<Category />} />
+              <Route path="/playing" element={<Playing />} />
+            </Route>
+            <Route path="*" element={<Login />} />
+          </Routes>
+        </PlayerProvider>
       </TokenProvider>
     </BrowserRouter>
   </React.StrictMode>
