9ab9f8bd37e02379df8e172aa0d7003320d5cfc3
cellle <91531988+cellle@users.noreply.github.com>
2022-10-05 14:36:11 +0200

    wip

--- src/pages/playing/index.tsx
+++ src/pages/playing/index.tsx
@@ -1,5 +1,22 @@
+import { usePlayer } from "../../contexts/PlayerContext";
+import AudioPlayer from "react-h5-audio-player";
+// import "react-h5-audio-player/lib/styles.css";
+
 function Playing() {
-  return <div>Playing</div>;
+  const { player } = usePlayer();
+  console.log(player.href);
+
+  return (
+    <div>
+      <p>{player.name}</p>
+      <p>{player.artists}</p>
+      <AudioPlayer
+        autoPlay
+        src={player.href ?? ""}
+        onPlay={(e) => console.log("onPlay")}
+      />
+    </div>
+  );
 }
 
 export default Playing;
