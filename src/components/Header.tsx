f4d513c3875b6f6313d690879433ac61531db586
cellle <91531988+celinerh@users.noreply.github.com>
2022-10-09 20:43:37 +0200

    add link to search icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { IoIosArrowBack } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { IoSearchOutline, IoLogOutOutline } from "react-icons/io5";
 
 function Header({ title }: { title: string }) {
@@ -26,7 +26,10 @@ function Header({ title }: { title: string }) {
       <h1 className="col-start-2 tracking-widest uppercase font-extralight place-self-center">
         {title}
       </h1>
-      <IoSearchOutline className="ml-auto" />
+
+      <NavLink to="/search">
+        <IoSearchOutline className="ml-auto" />
+      </NavLink>
     </div>
   );
 }
