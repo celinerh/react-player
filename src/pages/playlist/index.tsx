f95a03332cefce8ecd79e8e22d4f51bdfd1bfa19
cellle <91531988+cellle@users.noreply.github.com>
2022-10-10 11:05:43 +0200

    refactor how to get tracks from playlist

--- src/pages/playlist/index.tsx
+++ src/pages/playlist/index.tsx
@@ -1,29 +1,35 @@
-import { useParams } from "react-router-dom";
+import PlaylistCover from "../../components/PlaylistCover";
 import Title from "../../components/Title";
 import Track from "../../components/Track";
-import useGetTracks from "../../hooks/useGetTracks";
+import usePlaylist from "../../hooks/usePlaylist";
 
 function Playlist() {
-  const { id } = useParams();
-  const { tracks } = useGetTracks(
-    `https://api.spotify.com/v1/playlists/${id}/tracks`
-  );
+  const { playlist } = usePlaylist();
 
   return (
-    <div className="page-wrapper">
-      <Title title="All Songs" />
+    <div className="p-0 page-wrapper">
+      {playlist && (
+        <PlaylistCover
+          imageSource={playlist?.images[0].url}
+          playlistName={playlist?.name}
+        />
+      )}
 
-      <div className="flex flex-col gap-3">
-        {tracks &&
-          tracks.items.map((track: any) => (
-            <Track
-              key={track.id}
-              name={track.track.name}
-              trackURL={track.track.preview_url}
-              artists={track.track.artists.map((artist: any) => artist.name)}
-              duration={track.track.duration_ms}
-            />
-          ))}
+      <div className="p-6">
+        <Title title="All Songs" />
+
+        <div className="flex flex-col gap-3">
+          {playlist &&
+            playlist.tracks.items.map((track: any) => (
+              <Track
+                key={track.id}
+                name={track.track.name}
+                trackURL={track.track.preview_url}
+                artists={track.track.artists.map((artist: any) => artist.name)}
+                duration={track.track.duration_ms}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
